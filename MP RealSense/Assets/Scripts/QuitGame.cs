695e266034601c4c710643a192e6531a47ba30a5
ngcheehean <brandonngcheehean@gmail.com>
2023-08-04 14:23:08 +0800

    pause menu completed

--- MP RealSense/Assets/Scripts/QuitGame.cs
+++ MP RealSense/Assets/Scripts/QuitGame.cs
@@ -3,18 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseAndResumeGame : MonoBehaviour
+public class QuitGame : MonoBehaviour
 {
-    public void Pause()
-    {
-        Time.timeScale = 0;
-    }
-
-    public void Resume()
-    {
-        Time.timeScale = 1;
-    }
-
     public void Quit()
     {
         SceneManager.LoadScene("MenuScene");
