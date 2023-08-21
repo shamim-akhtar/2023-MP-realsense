39d8f365c7b2ca6aec693a99dc1b16d4182d9dba
ngcheehean <brandonngcheehean@gmail.com>
2023-08-21 10:32:59 +0800

    SpawnCubes spawns cubes

--- MP RealSense/Assets/Scripts/SpawnCubes.cs
+++ MP RealSense/Assets/Scripts/SpawnCubes.cs
@@ -4,26 +4,24 @@ using UnityEngine;
 
 public class SpawnCubes : MonoBehaviour
 {
-    GameObject cubes;
+    public GameObject cubes;
     public Transform[] spawnPoints;
     float spawnInterval;
 
     // Start is called before the first frame update
     void Start()
     {
-        cubes = GetComponent<GameObject>();
+        StartCoroutine(Spawn());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Spawn()
     {
-        spawnInterval = Random.Range(1, 5);
+        while (true)
+        {
+            spawnInterval = Random.Range(1.0f, 5.0f);
+            yield return new WaitForSeconds(spawnInterval);
 
-    }
-
-    void Spawn()
-    {
-        Instantiate(cubes, spawnPoints[Random.Range(0, 2)]);
-
+            Instantiate(cubes, spawnPoints[Random.Range(0, 2)]);
+        }
     }
 }
