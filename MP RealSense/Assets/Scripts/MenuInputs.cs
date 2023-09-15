81fdde6073a2d17ef7dbf509be00e1c02e5d43ee
ngcheehean <brandonngcheehean@gmail.com>
2023-09-15 10:26:28 +0800

    Added two modes

--- MP RealSense/Assets/Scripts/MenuInputs.cs
+++ MP RealSense/Assets/Scripts/MenuInputs.cs
@@ -6,13 +6,22 @@ using UnityEngine.SceneManagement;
 
 public class MenuInputs : MonoBehaviour
 {
-    public void StartButtonPressed()
+    public void EndlessButtonPressed()
     {
         // Unload the current scene
         SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
 
         // Load the target scene
         SceneManager.LoadScene("HandTrackGame 1");
+
+        Time.timeScale = 1;
+    }
+
+    public void TimedButtonPressed()
+    {
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+
+        SceneManager.LoadScene("HandTrackGame 2");
 
         Time.timeScale = 1;
     }
